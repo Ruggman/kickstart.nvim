4b3720b58a11976be660c6752f04fd6f1e8628b6
Michael Ruggles <ruggl015@gmail.com>
2025-01-28 16:08:43 -0600

    better git signs that I yanked from the help boiiii

--- init.lua
+++ init.lua
@@ -246,11 +246,12 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '|' },
-        change = { text = '|' },
+        add = { text = '┃' },
+        change = { text = '┃' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
+        untracked = { text = '┆' },
       },
       current_line_blame = true,
     },
